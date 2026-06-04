ac7d0c5a54368dccaef556f416a070aac43860af
Jatin Rajput <jatinrajput9845@gmail.com>
2026-06-04 12:28:54 +0530

    Changes for PR Comments.

--- src/main/java/in/sp/main/dto/RechargeDTO.java
+++ src/main/java/in/sp/main/dto/RechargeDTO.java
@@ -1,7 +1,12 @@
 package in.sp.main.dto;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@RequiredArgsConstructor
 public class RechargeDTO {
 
     private Long customerId;
@@ -16,54 +21,4 @@ public class RechargeDTO {
 
     private Long rechargedByEmployeeId;
 
-    public RechargeDTO() {
-    }
-
-    public Long getCustomerId() {
-        return customerId;
-    }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
-    }
-
-    public Long getServiceId() {
-        return serviceId;
-    }
-
-    public void setServiceId(Long serviceId) {
-        this.serviceId = serviceId;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public String getPaymentMode() {
-        return paymentMode;
-    }
-
-    public void setPaymentMode(String paymentMode) {
-        this.paymentMode = paymentMode;
-    }
-
-    public LocalDate getValidityTill() {
-        return validityTill;
-    }
-
-    public void setValidityTill(LocalDate validityTill) {
-        this.validityTill = validityTill;
-    }
-
-    public Long getRechargedByEmployeeId() {
-        return rechargedByEmployeeId;
-    }
-
-    public void setRechargedByEmployeeId(Long rechargedByEmployeeId) {
-        this.rechargedByEmployeeId = rechargedByEmployeeId;
-    }
 }
--- src/main/java/in/sp/main/schedulers/ReminderScheduler.java
+++ src/main/java/in/sp/main/schedulers/ReminderScheduler.java
@@ -5,12 +5,18 @@ import org.springframework.stereotype.Component;
 
 import in.sp.main.service.CustomerServiceManagement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * A scheduler to send email.
  */
+//@Slf4j
 @Component
 public class ReminderScheduler {
-
+    private static final Logger log =
+            LoggerFactory.getLogger(ReminderScheduler.class);
     private final CustomerServiceManagement
             customerServiceManagement;
 
@@ -22,10 +28,15 @@ public class ReminderScheduler {
                 customerServiceManagement;
     }
 
+    /**
+     * To send a ramiander emails to send to customers.
+     */
     @Scheduled(cron = "0 15 14 * * ?")
     public void runDailyReminder() {
-
+    log.info("Starting daily remainders schedulers");
         customerServiceManagement
                 .sendExpiryReminders();
+        log.info("Completed Schedule Process");
     }
+
 }
